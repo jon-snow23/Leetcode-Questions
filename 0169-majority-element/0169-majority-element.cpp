372cbd185545c3bd0dae5cc0c7e46fe0419de610
Vipin Nirmal <95864562+jon-snow23@users.noreply.github.com>
2023-03-22 16:03:22 +0530

    Time: 8 ms (99.32%), Space: 19.7 MB (38.20%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        return nums[nums.size()/2];
+        int count = 0 ;
+        int candidate = 0;
+        for(auto num : nums) {
+            if (count == 0 ){
+                candidate = num;
+            }
+            if (num==candidate) {
+                count+=1;
+            }else count-=1;
+        }
+        return candidate;
     }
 };
