0c0ccae0e05e033d59ca4c77b074434beea8d029
Vipin Nirmal <95864562+jon-snow23@users.noreply.github.com>
2024-03-05 23:23:34 +0530

    Time: 17 ms (98.28%), Space: 14 MB (13.45%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right && s[left] == s[right]) {
+            char target = s[left];
+            while (left <= right && s[left] == target) left++;
+            while (left <= right && s[right] == target) right--;
+        }
+        
+        return max(right - left + 1, 0);
+    }
+};
