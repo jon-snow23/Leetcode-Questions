e4d67f6fe3d6198777800683574c0dc2fd8869fc
Vipin Nirmal <95864562+jon-snow23@users.noreply.github.com>
2024-03-11 22:31:31 +0530

    Time: 2 ms (42.57%), Space: 9.3 MB (21.29%) - LeetHub

---
+++ 0765-couples-holding-hands/0765-couples-holding-hands.cpp
@@ -0,0 +1,21 @@
+ class Solution {
+public:
+    int minSwapsCouples(vector<int>& row) {
+       unordered_map<int, int>mp;
+       for(int i = 0; i<row.size(); i++) mp[row[i]] = i;
+       int count = 0;
+       for(int i = 0; i<row.size(); i+=2){
+           int first = row[i];
+           int second = first ^ 1;
+           if(row[i+1] != second){
+               count++;
+               int val = row[i+1];
+               int x = mp[second];
+               swap(row[i+1], row[x]);
+               mp[second] = i+1;
+               mp[val] = x;
+           }
+       } 
+       return count;
+    }
+};
