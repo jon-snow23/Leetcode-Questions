9979d9c05c0aa52b931b863ff50f7d49bac938d8
Vipin Nirmal <95864562+jon-snow23@users.noreply.github.com>
2024-07-04 09:46:40 +0530

    Time: 47 ms (47.45%), Space: 51 MB (23.16%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return false;
+        
+        int maxReach = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i > maxReach) return false;
+            maxReach = max(maxReach, i + nums[i]);
+            if (maxReach >= n - 1) return true; 
+        }
+        
+        return false;
+    }
+};
