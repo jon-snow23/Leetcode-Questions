32b7d4ee5db558655d5df7f90940e08926901a6d
Vipin Nirmal <95864562+jon-snow23@users.noreply.github.com>
2024-07-23 09:21:53 +0530

    Time: 5 ms (73.74%), Space: 12.9 MB (12.36%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,25 +1,17 @@
 class Solution {
+    int fun(int i , int j , vector<vector<int>> &grid , vector<vector<int>> &dp ) {
+        if (i==0 and j==0) return grid[i][j];
+        if(i<0 or j<0) return 1e9;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = grid[i][j] + fun(i-1 , j , grid , dp);
+        int left = grid[i][j] + fun(i , j-1 , grid , dp);
+        return dp[i][j] = min(up , left);
+    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m= grid.size(), n= grid[0].size(); 
-        vector<vector<int>>dp(m, vector<int>(n,0));
-        dp[0][0]= grid[0][0];
-        
-        //fill  first columns. 
-        for(int i=1;i<m;i++)
-            dp[i][0] = dp[i-1][0] + grid[i][0]; 
-        
-        //fill  first row. 
-        for(int i=1;i<n;i++)
-            dp[0][i] = dp[0][i-1] + grid[0][i];
-        
-        //fill rest rows and columns. 
-        for(int i=1;i<m;i++)
-            for(int j=1; j<n;j++)
-                dp[i][j]= grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
-        
-        //return ans;
-        return dp[m-1][n-1];
-        
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>dp(m , vector<int>(n,-1));
+        return fun(m-1 , n-1 , grid , dp);
     }
 };
