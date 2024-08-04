818fcfcb57303bca15423db1943378ed392feba5
Vipin Nirmal <95864562+jon-snow23@users.noreply.github.com>
2024-08-05 03:48:57 +0530

    Time: 78 ms (14.90%), Space: 72 MB (47.45%) - LeetHub

---
+++ 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<pair<int, vector<int>>> weightedRects;
+    long long totalWeight = 0;    
+    Solution(vector<vector<int>>& rects) {
+        for (auto& rect : rects) {
+            int area = (rect[2] - rect[0] + 1) * (rect[3] - rect[1] + 1);
+            totalWeight += area;
+            weightedRects.push_back(make_pair(area, rect));
+        }
+    }    
+    vector<int> pick() {
+        int weight = rand() % (totalWeight + 1);
+        for (auto& entry : weightedRects) {
+            if (weight <= entry.first) {
+                auto& rect = entry.second;
+                int px = rect[0] + rand() % (rect[2] - rect[0] + 1);
+                int py = rect[1] + rand() % (rect[3] - rect[1] + 1);
+                return {px, py};
+            }
+            weight -= entry.first;
+        }
+        return {};
+    }
+};
