7c944576475aec640529821fef7f7319aaf9ec81
Vipin Nirmal <95864562+jon-snow23@users.noreply.github.com>
2024-10-05 11:52:47 +0530

    Time: 44 ms (5.10%), Space: 18.6 MB (5.18%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,27 +1,30 @@
-class Solution {
+class Solution
+{
 public:
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        if(dungeon.size()==1 && dungeon[0].size()==1)
-        return -min(0, dungeon[0][0]) + 1;
-        vector<vector<int>> vec(dungeon.size(), vector<int> (dungeon[0].size(), INT_MAX));
-        // SORRY
-        return -min(max(recurse(dungeon, vec, 1,0), recurse(dungeon, vec, 0,1)) + dungeon[0][0], 0) + 1;
+    int calc(int i, int j, vector<vector<int>> &dungeon, map<string, int> &mp)
+    {
+        if (i >= dungeon.size() || j >= dungeon[0].size())
+            return INT_MAX;
+        string key = to_string(i) + "key" + to_string(j);
+        if (mp.contains(key))
+        {
+            return mp[key];
+        }
+        int health = min(calc(i + 1, j, dungeon, mp), calc(i, j + 1, dungeon, mp));
+
+        if (health == INT_MAX)
+            health = 1;
+
+        int res = max(health - dungeon[i][j], 1);
+        mp.insert({key, res});
+        return res;
     }
 
-    int recurse(vector<vector<int>>& dungeon, vector<vector<int>> &vec, int i, int j) {
-        if(i==dungeon.size() || j == dungeon[0].size())
-        return -INT_MAX;
-
-        if(vec[i][j] != INT_MAX) {
-            return vec[i][j];
-        }
-
-        int cur = max(recurse(dungeon, vec, i+1, j), recurse(dungeon, vec, i, j+1));
-
-        if(cur == -INT_MAX)
-        cur = 0;
-
-        vec[i][j] = min(0, cur+dungeon[i][j]);
-        return vec[i][j];
+    int calculateMinimumHP(vector<vector<int>> &dungeon)
+    {
+        if (dungeon.size() == 0 || dungeon[0].size() == 0)
+            return 0;
+        map<string, int> mp;
+        return calc(0, 0, dungeon, mp);
     }
 };
